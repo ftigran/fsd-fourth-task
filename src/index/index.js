fff18b5bbfed7da6ef16ffad2208ce1070cf05ae
Tigran Frangyan <ftigran1@gmail.com>
2020-10-05 07:07:30 +0300

    бегунок двигается, но без значений

--- src/index/index.js
+++ src/index/index.js
@@ -1,14 +1,25 @@
 import './index.scss'
 'use strict';
 
-
+/*
+Добавить:
+мининимально, максимального и текущего значения
+размер шага, 
+вертикальный/горизонтальный вид, 
+одиночное значение или интервал,
+возможность на лету изменить значение "снаружи" javascript-ом, 
+возможность включать/отключать элемент над бегунком, который показывает значение
+шкала значений - элемент, который отображает диапазон значений, в пределах которого можно двигать ползунок. Минимальное значение шкалы = значению min слайдера, максимальное = значению max слайдера. 
+Шкала должна быть интерактивной. Т.е. при клике на значение шкалы, ползунок должен перемещаться на позицию этого значения.
+прогресс бар - элемент от min до значения первого ползунка, при одиночном значении, либо, от значения первого ползунка до значения второго ползунка при интервале. Узнать как выглядит прогресс бар (разноцветные полоски)
+ */
 let view = {
-    createElements: function(elem,rangeValueFrom,rangeValueTo){
+    createElements: function(elem,rangeValueFrom,rangeValueTo, units){
         let rangeSlider = document.createElement('div');
         rangeSlider.className = 'range';
         let rangeValue = document.createElement('p');
         rangeValue.className = 'range__value';
-        rangeValue.innerHTML=rangeValueFrom + rangeValueTo;
+        rangeValue.innerHTML=rangeValueFrom+units+' - ' + rangeValueTo+units;
         rangeSlider.append(rangeValue);
 
 
@@ -26,7 +37,7 @@ let view = {
 
         let rangeFromNov = document.createElement('span');
         rangeFromNov.className = 'range__now-value';
-        rangeFromNov.innerHTML  = rangeValueFrom;
+        rangeFromNov.innerHTML  = rangeValueFrom+units;
         rangeFrom.append(rangeFromNov);
 
         let rangeTo = document.createElement('div');
@@ -34,7 +45,7 @@ let view = {
 
         let rangeToNov = document.createElement('span');
         rangeToNov.className = 'range__now-value';
-        rangeToNov.innerHTML=rangeValueTo;
+        rangeToNov.innerHTML=rangeValueTo+units;
         rangeTo.append(rangeToNov);
 
         rangeProgressBar.append(rangeFrom,rangeTo)
@@ -45,10 +56,39 @@ let view = {
     }
 };
 
-view.createElements(document.querySelector('.range-here'), 5000, 10000);
+view.createElements(document.querySelector('.range-here'), 5000, 10000, '₽');
 console.log('sdds')
 
+
+let rangeTo = document.querySelector('.range__to');
+let bar = document.querySelector('.range__progress-bar');
+let slider=document.querySelector('.range__border');
+let shiftX;
+
+rangeTo.onpointerdown = function(event) {
+    event.preventDefault(); // prevent selection start (browser action)
+    shiftX = event.clientX - rangeTo.getBoundingClientRect().left;
+
+    rangeTo.setPointerCapture(event.pointerId);
+  };
+  rangeTo.onpointermove = function(event) {
+    let newWidth = event.clientX - shiftX - slider.getBoundingClientRect().left;
+    // если указатель находится за пределами слайдера => отрегулировать "left", чтобы оставаться в пределах границ
+    if (newWidth < 0) {
+        newWidth = 0;
+    }
+    let rightEdge = slider.offsetWidth - rangeTo.offsetWidth;
+    if (newWidth > rightEdge) {
+        newWidth = rightEdge;
+    }
+
+    bar.style.width = newWidth + 'px';
+  };
+
+  rangeTo.ondragstart = () => false;
+
 let model={
+
 }
 let controller={
 
